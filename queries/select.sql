d654f91f78d9fb062413c620ac06895f50d51485
Yuhi-Sato <yuhi120101@gmail.com>
2024-12-06 07:49:23 +0000

    Revert "indexを沢山貼った"
    This reverts commit cf140d1c6e5943abab3bcb07ce3f52f4353d5ed9.

--- queries/select.sql
+++ queries/select.sql
@@ -9,9 +9,8 @@ SELECT * FROM player WHERE tenant_id=? ORDER BY created_at DESC
 SELECT * FROM competition WHERE tenant_id=? ORDER BY created_at DESC
 SELECT * FROM competition WHERE tenant_id = ? ORDER BY created_at ASC
 SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? AND player_id = ? ORDER BY row_num DESC LIMIT 1
-SELECT * FROM competition WHERE id IN (?)
 SELECT * FROM tenant WHERE id = ?
-SELECT ps.score as score, p.id as player_id, p.display_name as display_name, ps.row_num as row_num FROM player_score ps LEFT JOIN player p ON ps.player_id = p.id WHERE ps.tenant_id = ? AND ps.competition_id = ? ORDER BY ps.score DESC, ps.row_num ASC LIMIT ?
+SELECT * FROM player_score WHERE tenant_id = ? AND competition_id = ? ORDER BY row_num DESC
 SELECT * FROM competition WHERE tenant_id=? ORDER BY created_at DESC
 
 ----------------------------------------- back quote queries -----------------------------------------
