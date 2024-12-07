f1dd4ef2e42825b113c6ab752bd30ea63fd8998a
Yuhi-Sato <yuhi120101@gmail.com>
2024-12-07 16:17:01 +0900

    fgprof

--- go/cmd/isuports/main.go
+++ go/cmd/isuports/main.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	_ "net/http/pprof"
 
+	"github.com/felixge/fgprof"
 	isuports "github.com/isucon/isucon12-qualify/webapp/go"
 )
 
 func main() {
+	http.DefaultServeMux.Handle("/debug/fgprof", fgprof.Handler())
 	go func() {
-		log.Fatal(http.ListenAndServe(":6060", nil))
+		log.Fatal(http.ListenAndServe("localhost:6061", nil))
 	}()
 
 	isuports.Run()
