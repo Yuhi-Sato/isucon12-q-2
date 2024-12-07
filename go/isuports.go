8c5a9292f8997a2b652ac094762c1a345a6e9c7c
Yuhi-Sato <yuhi120101@gmail.com>
2024-12-07 16:06:13 +0900

    Revert "fix"
    This reverts commit d87cadcaf0e19a33afd042dfc4ebba18f38f3541.

--- go/isuports.go
+++ go/isuports.go
@@ -1369,23 +1369,18 @@ func playerHandler(c echo.Context) error {
 	}
 
 	// player_scoreを読んでいるときに更新が走ると不整合が起こるのでロックを取得する
-	// fl, err := flockByTenantID(v.tenantID)
-	// if err != nil {
-	// 	return fmt.Errorf("error flockByTenantID: %w", err)
-	// }
-	// defer fl.Close()
-	tx, err := tenantDB.Beginx()
+	fl, err := flockByTenantID(v.tenantID)
 	if err != nil {
-		return fmt.Errorf("error tenantDB.Beginx: %w", err)
+		return fmt.Errorf("error flockByTenantID: %w", err)
 	}
-	defer tx.Rollback()
+	defer fl.Close()
 	pss := make([]PlayerScoreRow, 0, len(cs))
 	for _, c := range cs {
 		ps := PlayerScoreRow{}
 		if val, ok := playerScoreByTenantIDCompetitionIDPlayerID.Load(fmt.Sprintf("%d:%s:%s", v.tenantID, c.ID, p.ID)); ok {
 			ps = val.(PlayerScoreRow)
 		} else {
-			if err := tx.GetContext(
+			if err := tenantDB.GetContext(
 				ctx,
 				&ps,
 				// 最後にCSVに登場したスコアを採用する = row_numが一番大きいもの
@@ -1417,7 +1412,7 @@ func playerHandler(c echo.Context) error {
 	query := "SELECT title FROM competition WHERE id IN (?)"
 	query, args, _ := sqlx.In(query, competitionIDs)
 	comps := []CompetitionRow{}
-	if err := tx.SelectContext(ctx, &comps, query, args...); err != nil {
+	if err := tenantDB.SelectContext(ctx, &comps, query, args...); err != nil {
 		return fmt.Errorf("error Select competition: %w", err)
 	}
 
