daee178ac77449c31f31cdfdcbaefaaaeee615c0
Yuhi-Sato <yuhi120101@gmail.com>
2024-12-07 11:37:38 +0900

    tenantのテーブル作成

--- sql/admin/10_schema.sql
+++ sql/admin/10_schema.sql
@@ -3,6 +3,9 @@ USE `isuports`;
 DROP TABLE IF EXISTS `tenant`;
 DROP TABLE IF EXISTS `id_generator`;
 DROP TABLE IF EXISTS `visit_history`;
+DROP TABLE IF EXISTS `competition`;
+DROP TABLE IF EXISTS `player`;
+DROP TABLE IF EXISTS `player_score`;
 
 CREATE TABLE `tenant` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -29,3 +32,34 @@ CREATE TABLE `visit_history` (
   `updated_at` BIGINT NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 ALTER TABLE `visit_history` ADD INDEX idx_tenant_id_competition_id_player_id_created_at (`tenant_id`, `competition_id`, `player_id`, `created_at`);
+
+CREATE TABLE competition (
+  `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+  `tenant_id` BIGINT NOT NULL,
+  `title` TEXT NOT NULL,
+  `finished_at` BIGINT NULL,
+  `created_at` BIGINT NOT NULL,
+  `updated_at` BIGINT NOT NULL
+);
+
+CREATE TABLE player (
+  `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+  `tenant_id` BIGINT NOT NULL,
+  `display_name` TEXT NOT NULL,
+  `is_disqualified` BOOLEAN NOT NULL,
+  `created_at` BIGINT NOT NULL,
+  `updated_at` BIGINT NOT NULL
+);
+
+CREATE TABLE player_score (
+  `id` VARCHAR(255) NOT NULL PRIMARY KEY,
+  `tenant_id` BIGINT NOT NULL,
+  `player_id` VARCHAR(255) NOT NULL,
+  `competition_id` VARCHAR(255) NOT NULL,
+  `score` BIGINT NOT NULL,
+  `row_num` BIGINT NOT NULL,
+  `created_at` BIGINT NOT NULL,
+  `updated_at` BIGINT NOT NULL
+);
+CREATE INDEX idx_tenant_id_player_id_competition_id ON player_score(`tenant_id`, `player_id`, `competition_id`);
+CREATE INDEX idx_score_desc_row_num ON player_score(`score` DESC, `row_num`);
